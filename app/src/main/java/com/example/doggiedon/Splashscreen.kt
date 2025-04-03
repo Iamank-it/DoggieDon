08d9e1f1e7b0392b2514cffc155df135363cc5ec
Ankit Kumar <182338442+Iamank-it@users.noreply.github.com>
2025-04-04 01:45:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/doggiedon/Splashscreen.kt
@@ -0,0 +1,33 @@
+package com.example.doggiedon
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.doggiedon.register.welcome
+
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splashscreen)
+
+        //for using this activity as splash screen
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,welcome::class.java))
+            finish()
+        },3000)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
